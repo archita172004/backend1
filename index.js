5c6e93f7a48d1ecdddc69ca676daa9c0c36a6a2f
Archita Chaudhary <archichaudhary2004@gmail.com>
2024-09-15 22:48:50 +0530

    a

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-require("dotenv").config();
-
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/ask", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/twitter", (req, res) => {
+app.get("/ask/twitter", (req, res) => {
   res.send("architachaudha5");
 });
 
-app.get("/login", (req, res) => {
+app.get("/ask/login", (req, res) => {
   res.send("<h1>please login </h1>  ");
 });
 
-app.get("/youtube", (req, res) => {
+app.get("/ask/youtube", (req, res) => {
   res.send("<h2>youtube</h2> ");
 });
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
